63660284c01be1ed9f3637c0581e7473e6a80b1b
kokeshiM0chi <frogandfrog009@gmail.com>
2025-02-10 11:24:21 +0900

    tmp

---
+++ nlp/set_envrionment.sh
@@ -0,0 +1,23 @@
+
+
+
+# apt-get -y install fonts-ipafont-gothic
+# brew install --cask font-ipafont
+# brew install wget
+
+pip install transformers
+pip install 'datasets==v2.11.0'
+
+wget https://github.com/ids-cv/wrime/raw/master/wrime-ver1.tsv
+
+
+pip install matplotlib
+pip install seaborn
+pip install fugashi ipadic
+pip install unidic_lite
+pip install scikit-learn
+
+
+
+
+pip install 'accelerate>=0.26.0'
